88f24ace47cf4e5ea55c79f2fe3afb3dc15c4da7
Anatoliy Klots and Brian Fan <anatoliy@>
2017-05-08 09:50:22 -0700

    add practice active record

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+ENV['RACK_ENV'] = 'test'
+
+require("rspec")
+require("pg")
+require("sinatra/activerecord")
+require("./lib/task")
+require("./lib/list")
+require('rake')
+
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Task.all().each() do |task|
+      task.destroy()
+    end
+    List.all().each() do |list|
+      list.destroy()
+    end
+  end
+end
